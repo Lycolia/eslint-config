48832f4e93daa399be5231ae2b8d750a9904d2bf
Lycolia Rizzim <lycolia@lycolia.info>
2023-06-09 01:57:32 +0900

    translate comment

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 module.exports = {
   rules: {
+    // トリプルイコールの強制
     eqeqeq: ['error', 'always'],
-    // Enforce the consistent use of either backticks, double, or single quotes
+    // シングルクォートの強制
+    // シングルクォートが入った文字列はダブルクォートで解決される："'aaaa'"など
     quotes: ['error', 'single'],
-    // Enforce the consistent use of either function declarations or expressions
+    // 関数宣言の代わりに関数式の使用を強制
+    // アロー関数を許容
+    // アロー関数の強制はできないのでお気持ち程度。const hoge = functionを禁止するだけなので、function hogeは書ける
     'func-style': [
       'error',
       'expression',
@@ -11,22 +15,31 @@ module.exports = {
         allowArrowFunctions: true,
       },
     ],
-    // Enforces return statements in callbacks of array's methods
+    // Arrayメソッドのコールバックにreturnを強制
+    // mapなどをforEachのように使うことを防ぐことや、return漏れを防ぐのが目的
+    // forEachでのreturnを禁止
+    // returnの有無で使い分けするのが目的
     'array-callback-return': [
       'error',
       {
         checkForEach: true,
       },
     ],
-    // Allow using namespace
+    // 名前空間の使用を許可
     '@typescript-eslint/no-namespace': 'off',
-    // Allow non-explicit return types on functions and class methods
+    // 関数の戻り型を定義しないことを許可
+    // 毎回書くと冗長なので推論で済む場合は省略したい
     '@typescript-eslint/explicit-function-return-type': 'off',
-    // Allow non-explicit return types on exported functions
+    // exportされた関数の戻り型を定義しないことを許可
+    // 毎回書くと冗長なので推論で済む場合は省略したい
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    // Disallow usage of the any type
+    // anyの使用を許容しない
     '@typescript-eslint/no-explicit-any': 1,
-    // Restricts the types allowed in boolean expressions
+    // Boolean型以外をBoolean的に扱うことの禁止
+    // !''や!0のようなケースを阻止する
+    // 以下を防ぐのが目的。基本はeqeqeqと同じ信念
+    // - optionalをBoolean的に扱った時に非optional化した時の誤作動
+    // - 期待値の考慮漏れによる誤作動
     '@typescript-eslint/strict-boolean-expressions': ['error', {
       'allowString': false,
       'allowNumber': false,
@@ -37,14 +50,17 @@ module.exports = {
       'allowAny': false,
       'allowRuleToRunWithoutStrictNullChecksIKnowWhatIAmDoing': false
     }],
-    // Disallows explicit type declarations for variables or parameters initialized to a number, string, or boolean
+    // 次の型変数を初期化する時に型を書くことを許容しない
+    // number, string, boolean
+    // コードの冗長化防止
+    // 但し関数の仮引数は許可する（型があったりなかったりすると読みづらいと感じるため）
     '@typescript-eslint/no-inferrable-types': [
       'error',
       {
         ignoreParameters: true,
       },
     ],
-    // Error on declared but not used variable
+    // 未使用の変数を許容しない
     '@typescript-eslint/no-unused-vars': 'error',
   },
 };
